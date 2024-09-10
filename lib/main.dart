a161632c9bbf929c55e40793a6d43a8e5c5f2f89
abghifareihand <info.abghifareihan@gmail.com>
2024-09-10 16:01:11 +0700

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test_itdigital/presentation/login/pages/login_page.dart';
 import 'package:flutter_test_itdigital/presentation/todo/cubit/page_cubit.dart';
-import 'package:flutter_test_itdigital/presentation/todo/pages/todo_free_page.dart';
 
 void main() {
   runApp(const MyApp());
